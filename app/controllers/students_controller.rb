8a9be2103b9881881acd2d4df1626da30973f0ae
billymwangi <billy.mwangi@student.moringaschool.com>
2022-09-25 18:57:34 +0300

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,10 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+    
   end
-
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
